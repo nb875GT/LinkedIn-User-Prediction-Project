7f835489011f8741dd25cb93a24ac6be69993047
nb875GT <120539844+nb875GT@users.noreply.github.com>
2022-12-14 00:39:18 -0600

    Update linkedin_webapp.py

--- linkedin_webapp.py
+++ linkedin_webapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+pip install --upgrade pip
 import os
 os.chdir('C:\\Users\\nickb\\Python')
 from sklearn.linear_model import LogisticRegression
